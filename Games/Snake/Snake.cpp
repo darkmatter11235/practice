0822e5f723311a23c2e1d7e1c02566101360237d
Dark Matter <darkmatter112358@gmail.com>
2017-03-12 10:00:22 -0700

    added ncurses basic

--- Games/Snake/Snake.cpp
+++ Games/Snake/Snake.cpp
@@ -1,34 +1,113 @@
 #include "Snake.h"
 
+#include<unistd.h>
+#include<ncurses.h>
+#include<term.h>
+
 #include<iostream>
 #include<cmath>
 #include<cstdlib>
 
+
 void SnakeGui::DrawCanvas() {
     //Draw the walls
+    clear();
+    char w = '#';
+    char space = ' ';
+    char fruit = 'O';
+    char S = 'o';
     for(int i = 0; i < height; i++) {
         if ( i == 0 || i == height-1 ) {
             for(int j = 0; j < width; j++) {
-                std::cout << "#";
+                printw("%c", w);
             }
         } else {
-            std::cout << "#";
-            for(int i = 0; i < width-2; i++) {
-                std::cout << "";
+            printw("%c", w);
+            for(int j = 0; j < width-2; j++) {
+                if ( i == fruitY && j == fruitX ) {
+                    printw("%c", fruit);
+                } else if ( i == snakeX && j == snakeY ) {
+                    printw("%c", S);
+                } else {
+                    printw("%c", space);
+                }
             }
-            std::cout << "#";
+            printw("%c", w);
         }
-        std::cout << endl;
+        printw("\n");
     }
 }
 
 void SnakeGui::GetUserInput() {
+    int c;
+    c = getch();
+    switch(c) {
+        case 'j':
+            nextMove = ML;
+            break;
+        case 'l':
+            nextMove = MR;
+            break;
+        case 'i':
+            nextMove = MU;
+            break;
+        case 'k':
+            nextMove = MD;
+            break;
+        default:
+            nextMove = NM;
+            break;
+    }
+    if ( nextMove != NM ) {
+        
+    }
+    return ;
+}
+
+bool SnakeGui::GameOver() {
+    if ( gameOver ) {
+        return true;
+    }
+    return false;
 }
 
 void SnakeGui::UpdateCanvas() {
+
+    switch(nextMove) {
+        case ML:
+            snakeX--;
+            break;
+        case MR:
+            snakeX++;
+            break;
+        case MU:
+            snakeY++;
+            break;
+        case MD:
+            snakeY--;
+            break;
+        default:
+            break;
+    }
+        
 }
 
-int main(cha** argc, int argv) {
-    SnakeGui sg = new SnakeGui();
-    sg.DrawCanvas();
+int main(int argv, char** argc) {
+    int cw, ch;
+    cw = ch = 40;
+    SnakeGui sg(cw, ch);
+    //newterm();
+    initscr();
+    raw();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    while(!sg.GameOver()) {
+        sg.DrawCanvas();
+        sg.GetUserInput();
+        sg.UpdateCanvas();
+        refresh();
+    }
+    endwin();
+    return 0;
 }
--- Games/Snake/Snake.h
+++ Games/Snake/Snake.h
@@ -1,20 +1,26 @@
 #ifndef GAMES_SNAKE_H
 #define GAMES_SNAKE_H
+
+enum MOVE {ML =0, MR, MU, MD, NM };
 class SnakeGui {
     public:
         SnakeGui(int w, int h): width(w), height(h) {
-            sx = w/2;
-            sy = h/2;
+            snakeX = w/2;
+            snakeY = h/2;
+            gameOver = false;
         }
         void DrawCanvas();
         void GetUserInput();
         void UpdateCanvas();
+        bool GameOver();
     private:
         int height;
         int width;
-        int sx;
-        int sy;
+        int snakeX;
+        int snakeY;
         int fruitX;
         int fruitY;
+        bool gameOver;
+        MOVE nextMove;
 };
 #endif
