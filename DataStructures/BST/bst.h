dfdeabb55d2cfaf11d0591cb567fd88b3b4887e8
Dark Matter <darkmatter112358@gmail.com>
2017-03-17 08:54:22 -0700

    adding DP

--- DataStructures/BST/bst.h
+++ DataStructures/BST/bst.h
@@ -1,7 +1,44 @@
 #ifndef PROJECT_BST_H
 #define PROJECT_BST_H
 
-#include<iostream>
 
+template<typename T> class bsTree {
+	private:
+		class bsNode {
+			public:
+				bsNode() *val(new T), leftChild(nullptr), rightChild(nullptr) {}
+				bsNode(T inVal) val(inVal), leftChild(nullptr), rightChild(nullptr) {}
+
+				T getValue() { return val; }
+				bsNode<T> *getLeft() { return leftChild; }
+				bsNode<T> *getRight() { return leftChild; }
+
+			private:
+				T val;
+				bsNode<T> *leftChild;
+				bsNode<T> *rightChild;
+		};
+		bsNode *root;
+	public:
+		//constructor
+		bsTree() root(NULL) {}
+
+		//destructor
+		~bsTree();
+
+		//display
+		void printTree();
+
+		//basic operations
+		void insert(T val);
+		bool remove(T val);
+		remove find(T val);
+
+		//traversals
+		void inOrder();
+		void preOrder();
+		void postOrder();
+
+};
 
 #endif
